f4cd4ae16af937dd48b74287a1dfe2211f0dbcae
LuLuLuann <lu.akaydin@gmail.com>
2025-01-17 19:26:48 -0500

    updated readme

--- example.mjs
+++ example.mjs
@@ -11,7 +11,7 @@ const n4 = 5;
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
-// Check two: at least two odd numbers
+// Check two: at least two odd numbers.
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
